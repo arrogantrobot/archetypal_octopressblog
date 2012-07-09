efb5bb3b550ab86357cba88c4427d640ceea8219
Rob Long <robert.l.long@gmail.com>
2012-07-08 20:30:20 -0500

    added details to about sidebar

---
+++ update.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+system("rake generate");
+
+system("git st");
+
+system("git add .");
+
+my $message;
+while( ! $message) {
+    print "Enter commit message: ";
+    chomp($message = <>);
+    if (not $message) {
+        print "no message received, please enter a commit message.\n";
+    }
+}
+system("git commit -m \"$message\"");
+system("git push heroku master");
+
+print "Done updating.\n"
